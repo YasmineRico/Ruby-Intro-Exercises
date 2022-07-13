65ba7a69b7077aaf6d38748966c80fef21a7bdc7
YasmineRico <ricolyasmine@gmail.com>
2022-07-13 17:33:50 -0500

    work through arrays exercises

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -15,22 +15,25 @@ end
 
 def last_three_elements(array)
   # return the last 3 elements of the array
+  array.last(3)
+
 end
 
 def add_element(array)
   # add an element (of any value) to the array
+  array << 4
 end
 
 def remove_last_element(array)
   # Step 1: remove the last element from the array
-
+  array.pop
   # Step 2: return the array (because Step 1 returns the value of the element removed)
   array
 end
 
 def remove_first_three_elements(array)
   # Step 1: remove the first three elements
-
+    array.shift(3)
   # Step 2: return the array (because Step 1 returns the values of the elements removed)
   array
 end
